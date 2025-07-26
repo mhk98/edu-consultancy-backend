d4c9bb92c243257ac62275fca1066aa76c02f8c2
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2025-07-26 13:38:37 +0600

    first commit

--- app/modules/requirement/requirement.routes.js
+++ app/modules/requirement/requirement.routes.js
@@ -1,7 +1,7 @@
 const { ENUM_USER_ROLE } = require("../../enums/user");
 const auth = require("../../middlewares/auth");
 const { uploadSingle } = require("../../middlewares/upload");
-const RequirementController = require("./Requirement.controller");
+const RequirementController = require("./requirement.controller");
 const router = require("express").Router();
 
 router.post("/create", uploadSingle, RequirementController.insertIntoDB);
